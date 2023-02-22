9b32997e4d15b131c6d172ccb42957dd2c66b7ff
Michael <michael.ejim@prunny.com>
2023-02-22 14:29:41 +0100

    Basic Auth util method

---
+++ src/main/java/First/Spring/util/AppUtil.java
@@ -0,0 +1,11 @@
+package First.Spring.util;
+
+import java.util.Base64;
+
+public class AppUtil {
+
+    private static final String getBasicAuthenticationHeader(String username, String password) {
+        String valueToEncode = username + ":" + password;
+        return "Basic " + Base64.getEncoder().encodeToString(valueToEncode.getBytes());
+    }
+}
